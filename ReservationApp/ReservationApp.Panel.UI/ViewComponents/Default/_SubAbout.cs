3a6b82a2d7967e4562fd150d89e89d357b189a4b
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-08-10 15:50:10 +0300

    Add new theme and homepage view component

---
+++ ReservationApp/ReservationApp.Panel.UI/ViewComponents/Default/_SubAbout.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ReservationApp.Panel.UI.ViewComponents.Default
+{
+    public class _SubAbout:ViewComponent
+    {
+        SubAboutManager subAboutManager = new SubAboutManager(new EfSubAboutDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = subAboutManager.TGetList();
+            return View(values);
+        }
+    }
+}
---
+++ ReservationApp/ReservationApp.Panel.UI/ViewComponents/Default/_Testimonial.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol;
+
+namespace ReservationApp.Panel.UI.ViewComponents.Default
+{
+    public class _Testimonial:ViewComponent
+    {
+        TestimonialManager testimonialManager = new TestimonialManager(new EfTestimonialDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = testimonialManager.TGetList();
+            return View(values);
+        }
+    }
+}
