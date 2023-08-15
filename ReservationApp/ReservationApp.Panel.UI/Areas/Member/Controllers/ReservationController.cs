b955b7a383cc444f24621055e30136b69fc41a25
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-08-15 23:23:00 +0300

    Create reservation page

---
+++ ReservationApp/ReservationApp.Panel.UI/Areas/Member/Controllers/ReservationController.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ReservationApp.Panel.UI.Areas.Member.Controllers
+{
+    [AllowAnonymous]
+    [Area("Member")]
+    public class ReservationController : Controller
+    {
+        DestinationManager dm = new DestinationManager(new EfDestinationDal());
+        ReservationManager rm = new ReservationManager(new EfReservationDal());
+        public IActionResult MyCurrentReservation()
+        {
+            return View();
+        }
+
+        public IActionResult MyOldReservation()
+        {
+            return View();
+        }
+
+        public IActionResult NewReservation()
+        {
+            List<SelectListItem> values = (from x in dm.TGetList()
+                                           select new SelectListItem
+                                           {
+                                            Text=x.City,
+                                            Value=x.DestinationID.ToString()
+                                           }).ToList();
+            ViewBag.v = values;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewReservation(Reservation p)
+        {
+            p.AppUserId = 5;
+            rm.TAdd(p);
+
+            return View("MyCurrentReservation");
+        }
+    }
+}
