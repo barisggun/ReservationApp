dec5955801e6d083cb5047fd880cc9f2ff1f7adc
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-08-25 17:35:22 +0300

    Add admin layout

--- ReservationApp/ReservationApp.Panel.UI/ViewComponents/MemberDashboard/_GuideList.cs
+++ ReservationApp/ReservationApp.Panel.UI/ViewComponents/MemberDashboard/_GuideList.cs
@@ -1,6 +1,18 @@
-﻿namespace ReservationApp.Panel.UI.ViewComponents.MemberDashboard
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ReservationApp.Panel.UI.ViewComponents.MemberDashboard
 {
-    public class _GuideList
+    public class _GuideList:ViewComponent
     {
+        GuideManager gm = new GuideManager(new EfGuideDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = gm.TGetList();
+            return View(values);
+        }
     }
 }
---
+++ ReservationApp/ReservationApp.Panel.UI/ViewComponents/MemberDashboard/_PlatformSetting.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReservationApp.Panel.UI.ViewComponents.MemberDashboard
+{
+    public class _PlatformSetting:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+
+    }
+}
