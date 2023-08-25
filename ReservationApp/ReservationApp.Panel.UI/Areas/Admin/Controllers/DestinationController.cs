7565305e26d7859e72ba323fac4a64f075058a72
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-08-26 00:53:57 +0300

    Add destination crud to admin dashboard

--- ReservationApp/ReservationApp.Panel.UI/Areas/Admin/Controllers/DestinationController.cs
+++ ReservationApp/ReservationApp.Panel.UI/Areas/Admin/Controllers/DestinationController.cs
@@ -1,13 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ReservationApp.Panel.UI.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [AllowAnonymous]
     public class DestinationController : Controller
     {
+        DestinationManager dm = new DestinationManager(new EfDestinationDal());
+
         public IActionResult Index()
         {
+            var values = dm.TGetList();
+            return View(values);
+        }
+
+        public IActionResult AddDestination()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult AddDestination(Destination destination)
+        {
+            dm.TAdd(destination);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteDestination(int id)
+        {
+            var values = dm.TGetById(id);
+            dm.TDelete(values);
+            return View("Index","Destination");
+        }
+        public IActionResult UpdateDestination(int id) 
+        {
+            var values = dm.TGetById(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdateDestination(Destination destination)
+        {
+            dm.TUpdate(destination);
+            return View("Index");
         }
     }
 }
