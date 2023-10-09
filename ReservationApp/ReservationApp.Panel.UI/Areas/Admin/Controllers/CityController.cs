bdce554e570e121a7962ed21c709287cf181f183
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-10-09 11:07:06 +0300

    Add city pages ajax

---
+++ ReservationApp/ReservationApp.Panel.UI/Areas/Admin/Controllers/CityController.cs
@@ -0,0 +1,52 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ReservationApp.Panel.UI.Models;
+
+namespace ReservationApp.Panel.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CityController : Controller
+    {
+        private readonly IDestinationService _destinationService;
+
+        public CityController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CityList()
+        {
+            var jsonCity = JsonConvert.SerializeObject(_destinationService.TGetList());
+            return Json(jsonCity);
+        }
+
+        public static List<CityClass> cities = new List<CityClass>
+        {
+            new CityClass
+            {
+                CityID = 1,
+                CityName = "Üsküp",
+                CityCountry ="Makedonya"
+            },
+            new CityClass
+            {
+                CityID = 2,
+                CityName ="Roma",
+                CityCountry ="İtalya"
+            },
+            new CityClass
+            {
+                CityID = 3,
+                CityName = "Istanbul",
+                CityCountry = "Turkey"
+            }
+        };
+
+    }
+}
